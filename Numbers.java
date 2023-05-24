74085808afd2e6a4cc98c2bb882228e91cf80ae7
Fatima.Cydeo <essaborfatimazahraa@gmail.com>
2023-05-24 15:18:05 -0400

    Added Numbers class

---
+++ Numbers.java
@@ -0,0 +1,17 @@
+public class Numbers {
+    public static void main(String[] args) {
+        int num = 8;
+        System.out.println(OddOrEven(num));
+    }
+
+    public static String OddOrEven (int num){
+        if(num %2 ==0){
+            System.out.println(num + " is an even number ");
+            return "Even";
+        }else{
+            System.out.println(num + " is an odd number");
+
+        }
+        return "Odd";
+    }
+}
